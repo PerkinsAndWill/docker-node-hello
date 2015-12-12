638af69c009fef0684b74882863d00e433a9c818
root <root@dev-box-1.ngcyc55o0oputmsbcdso3kzlac.bx.internal.cloudapp.net>
2015-12-12 20:08:20 +0000

    Updates to Dockerfile and index.js: docker running correctly now

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World\n');
+  res.send('Great Docker success.\n');
+});
+app.get('/test/',function(req,res){
+  res.send('Great Docker/test success.\n');
 });
 
 app.listen(PORT)
